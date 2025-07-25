70466707bad11fc42abf5a4e5733680447098f56
CJ Ordog <cjordog@gmail.com>
2025-07-24 19:29:48 -0700

    Initial commit

---
+++ ModTemplate/Plugin.cs
@@ -0,0 +1,174 @@
+﻿using BepInEx.Unity.IL2CPP.Utils;
+using BepInEx.Unity.IL2CPP;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using BepInEx.Configuration;
+using ModTemplate.Plugins;
+using UnityEngine;
+using System.Collections;
+using SaveProfileManager.Plugins;
+using System.Reflection;
+
+namespace ModTemplate
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, ModName, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BasePlugin
+    {
+        public const string ModName = "ModTemplate";
+
+        public static Plugin Instance;
+        private Harmony _harmony = null;
+        public new static ManualLogSource Log;
+
+        public ConfigEntry<bool> ConfigEnabled;
+        //public ConfigEntry<string> ConfigSongTitleLanguageOverride;
+        //public ConfigEntry<float> ConfigFlipInterval;
+
+
+
+        public override void Load()
+        {
+            Instance = this;
+
+            Log = base.Log;
+
+            SetupConfig(Config, Path.Combine("BepInEx", "data", ModName));
+            SetupHarmony();
+
+            var isSaveManagerLoaded = IsSaveManagerLoaded();
+            if (isSaveManagerLoaded)
+            {
+                AddToSaveManager();
+            }
+        }
+
+        // Any data that's likely to be shared between multiple profiles should use the dataFolder path
+        // Any data that's likely to be specific per profile should use the saveFolder path
+        private void SetupConfig(ConfigFile config, string saveFolder, bool isSaveManager = false)
+        {
+            string dataFolder = Path.Combine("BepInEx", "data", ModName);
+
+            if (!isSaveManager)
+            {
+                ConfigEnabled = config.Bind("General",
+                   "Enabled",
+                   true,
+                   "Enables the mod.");
+            }
+
+            //ConfigSongTitleLanguageOverride = config.Bind("General",
+            //    "SongTitleLanguageOverride",
+            //    "JP",
+            //    "Sets the song title to the selected language. (JP, EN, FR, IT, DE, ES, TW, CN, KO)");
+
+            //ConfigFlipInterval = config.Bind("General",
+            //    "FlipInterval",
+            //    3f,
+            //    "How quickly the difficulty flips between oni and ura.");
+        }
+
+        private void SetupHarmony()
+        {
+            // Patch methods
+            _harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+
+            LoadPlugin(ConfigEnabled.Value);
+        }
+
+        public static void LoadPlugin(bool enabled)
+        {
+            if (enabled)
+            {
+                bool result = true;
+                // If any PatchFile fails, result will become false
+                //result &= Instance.PatchFile(typeof(SwapJpEngTitlesPatch));
+                //result &= Instance.PatchFile(typeof(AdjustUraFlipTimePatch));
+                //SwapJpEngTitlesPatch.SetOverrideLanguages();
+                if (result)
+                {
+                    Logger.Log($"Plugin {MyPluginInfo.PLUGIN_NAME} is loaded!");
+                }
+                else
+                {
+                    Logger.Log($"Plugin {MyPluginInfo.PLUGIN_GUID} failed to load.", LogType.Error);
+                    // Unload this instance of Harmony
+                    // I hope this works the way I think it does
+                    Instance._harmony.UnpatchSelf();
+                }
+            }
+            else
+            {
+                Logger.Log($"Plugin {MyPluginInfo.PLUGIN_NAME} is disabled.");
+            }
+        }
+
+        private bool PatchFile(Type type)
+        {
+            if (_harmony == null)
+            {
+                _harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+            }
+            try
+            {
+                _harmony.PatchAll(type);
+#if DEBUG
+                Logger.Log("File patched: " + type.FullName);
+#endif
+                return true;
+            }
+            catch (Exception e)
+            {
+                Logger.Log("Failed to patch file: " + type.FullName);
+                Logger.Log(e.Message);
+                return false;
+            }
+        }
+
+        public static void UnloadPlugin()
+        {
+            Instance._harmony.UnpatchSelf();
+            Logger.Log($"Plugin {MyPluginInfo.PLUGIN_NAME} has been unpatched.");
+        }
+
+        public static void ReloadPlugin()
+        {
+            // Reloading will always be completely different per mod
+            // You'll want to reload any config file or save data that may be specific per profile
+            // If there's nothing to reload, don't put anything here, and keep it commented in AddToSaveManager
+            //SwapSongLanguagesPatch.InitializeOverrideLanguages();
+            //TaikoSingletonMonoBehaviour<CommonObjects>.Instance.MyDataManager.MusicData.Reload();
+        }
+
+        public void AddToSaveManager()
+        {
+            // Add SaveDataManager dll path to your csproj.user file
+            // https://github.com/Deathbloodjr/RF.SaveProfileManager
+            var plugin = new PluginSaveDataInterface(MyPluginInfo.PLUGIN_GUID);
+            plugin.AssignLoadFunction(LoadPlugin);
+            plugin.AssignUnloadFunction(UnloadPlugin);
+            //plugin.AssignReloadSaveFunction(ReloadPlugin);
+            plugin.AssignConfigSetupFunction(SetupConfig);
+            plugin.AddToManager();
+        }
+
+        private bool IsSaveManagerLoaded()
+        {
+            try
+            {
+                Assembly loadedAssembly = Assembly.Load("com.DB.RF.SaveProfileManager");
+                return loadedAssembly != null;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static MonoBehaviour GetMonoBehaviour() => TaikoSingletonMonoBehaviour<CommonObjects>.Instance;
+        public Coroutine StartCoroutine(IEnumerator enumerator)
+        {
+            return GetMonoBehaviour().StartCoroutine(enumerator);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,63 @@
+# RF.ModTemplate
+ A Rhythm Festival mod template for developing new mods.
+ 
+ <a href="taikomodmanager:insertGithubLinkhereAndReplaceWithUrlShortener"> <img src="Resources/InstallButton.png" alt="One-click Install using the Taiko Mod Manager" width="256"/> </a>
+ 
+# Requirements
+ Visual Studio 2022 or newer\
+ Taiko no Tatsujin: Rhythm Festival
+ 
+ 
+# How to use
+ On github, click the "Use this template" button -> Create a new repository\
+ (Optional) Check "Include all branches", which is just a "main" and "dev" branch\
+ Give a Repository name\
+ Select Private or Public repository\
+ Clone the repo locally
+
+ Change a few instances of "ModTemplate" to whatever you will name your mod <ModName>\
+ That would be:
+ - The main directory of this
+ - ModTemplate.sln
+    - Also open the .sln file in a text editor to change "ModTemplate", "ModTemplate\ModTemplate.csproj", replacing ModTemplate with <ModName>
+ - ModTemplate folder
+ - Within that folder, ModTemplate.csproj
+ 
+ Now open the solution itself\
+ In the .csproj file (double click the <ModName> project in solution explorer)
+ - Change ModName to your ModName
+ - Change the description to whatever your mod does (Or remember to do this later)
+ - Change ModVersion to a fitting version in the format of x.x.x (or whatever your preference is)
+
+ In Plugin.cs
+ - Change the namespace to what you selected as RootNamespace earlier
+    - In Visual Studio, you can highlight the namespace, hit Ctrl + R, Ctrl + R, and enter your new namespace to change all instances of it. 
+    - You can change the namespaces in the 2 example patches as well, to RootNamespace.Patches
+ - Change public const string ModName = your ModName
+
+ At this point, build the project, and it should have some errors
+ - Return to File Explorer and open the .csproj.user file
+ - Put your Rhythm Festival game install directory in GameDir
+ - Download or build the [SaveProfileManager](https://github.com/Deathbloodjr/RF.SaveProfileManager) mod, and place that dll full path in SaveProfileManagerPath
+
+ You project should now be in a buildable state
+ 
+ Outside of code, edit the README.md file
+ - Change RF.ModTemplate to your mod's name
+ - Change the description.
+ - Change the "href="taikomodmanager:insertGithubLinkhereAndReplaceWithUrlShortener" to a shortened url in the format of "taikomodmanager:https://github.com/<you\>/\<yourmodrepo\>", allowing the use of the One Click Mod Install button.
+ - Delete this "#How to use" section
+ - Make any other changes you feel like making
+ 
+
+# Build
+ Install [BepInEx 6.0.0-pre.2](https://github.com/BepInEx/BepInEx/releases/tag/v6.0.0-pre.2) into your Rhythm Festival directory and launch the game.\
+ This will generate all the dummy dlls in the interop folder that will be used as references.\
+ Make sure you install the Unity.IL2CPP-win-x64 version.\
+ Newer versions of BepInEx could have breaking API changes until the first stable v6 release, so those are not recommended at this time.
+ 
+ Attempt to build the project, or copy the .csproj.user file from the Resources file to the same directory as the .csproj file.\
+ Edit the .csproj.user file and place your Rhythm Festival file location in the "GameDir" variable.\
+ Download or build the [SaveProfileManager](https://github.com/Deathbloodjr/RF.SaveProfileManager) mod, and place that dll full path in SaveProfileManagerPath.
+
+Add BepInEx as a nuget package source (https://nuget.bepinex.dev/v3/index.json)
